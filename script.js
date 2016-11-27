d0d5e34a7c8974cbbcfe8971ace85f101dd07003
zhaidongfang <zhaidongfang@didichuxing.com>
2016-11-27 18:21:20 +0800

    a

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function scrollFunction(){
 }
 window.onscroll = scrollFunction;
 window.onresize = scrollFunction;
+
+// 判断是否支持video标签
+var hasVideo = !!(document.createElement('video').canPlayType);
+if(hasVideo){
+	var video = $('#supportVideo')[0];
+	// video.currentTime = 0;
+	$('#control').on('click',function(){
+		$(this).css('display','none');
+		video.play();
+	});
+}else{
+	alert('请使用支持HTML5的浏览器，如Chrome、Safari访问以查看视频')
+}
