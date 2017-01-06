b1fd88c39957d8b679f9d2e2ba96ff0cb75a431a
Benny Chia <bienfantaisie@gmail.com>
2017-01-06 18:40:20 +0800

    a

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function scrollFunction(){
-	var distance = $(document).scrollTop();
-    if(distance > 128){
-    	$('body').addClass('checked');
-    }else{
-    	$('body').removeClass('checked');
-    }
-}
-window.onscroll = scrollFunction;
-window.onresize = scrollFunction;
-
 // 判断是否支持video标签
 var hasVideo = !!(document.createElement('video').canPlayType);
 if(hasVideo){
